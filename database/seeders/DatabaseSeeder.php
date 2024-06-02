3469c4fcba078d036bfa740ea340a78cd5243a90
Kevin Daly <krd313@gmail.com>
2024-06-02 14:37:51 +1000

    first - after seed

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        User::factory()->create([
+            'name' => 'Test',
+            'email' => 'test@gmail.com',
+            'password' => bcrypt('password'),
+            'email_verified_at' => now(),
+        ]);
+
+        Project::factory()
+            ->count(30)
+            ->hasTasks(30)
+            ->create();
+    }
+}
