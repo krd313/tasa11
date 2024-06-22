0421a6a4b08796547607b2c7568710b4a015f967
Kevin Daly <krd313@gmail.com>
2024-06-22 10:47:09 +1000

    updated constant.jsx

--- resources/js/constants.jsx
+++ resources/js/constants.jsx
@@ -28,3 +28,13 @@ export const TASK_PRIORITY_TEXT_MAP = {
   medium: "Medium",
   high: "High",
 };
+export const USER_STATUS_CLASS_MAP = {
+  pending: "bg-amber-500",
+  in_progress: "bg-blue-500",
+  completed: "bg-green-500",
+};
+export const USER_STATUS_TEXT_MAP = {
+  pending: "Pending",
+  in_progress: "In Progress",
+  completed: "Completed",
+};
